17a467f7c0c9d2eec0e02a0b8d63dc8a5e4537ea
Reyner <reyenr1012002@gmail.com>
2024-07-08 01:15:42 -0600

    Agregando manejo de codigos QR + Pagina de exito(FALTA TRABJAR EN ELLA)

---
+++ app/exito/page.tsx
@@ -0,0 +1,7 @@
+export default function Success() {
+    return (
+        <div className="flex justify-center items-center mt-10">
+            <p className="text-2xl">Orden preparada con exito!</p>
+        </div>
+    );
+}
--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/orders/',
+    baseURL: 'https://d28f-179-50-186-244.ngrok-free.app/api/orders/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': 'true',
     },
 });
 
