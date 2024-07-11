6cc7e71f6382685ea0ef40cf51fa8452fd30c41e
Reyner <reyenr1012002@gmail.com>
2024-07-11 12:57:35 -0600

    url

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'http://209.38.172.75/api/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
