a66905ff3f5d2387d1eda79431319aac4bf6ed22
Reyner <reyenr1012002@gmail.com>
2024-07-18 23:31:30 -0600

    Agregando control de usuarios y seguridad

--- app/preparados/layout.tsx
+++ app/preparados/layout.tsx
@@ -6,8 +6,8 @@ export default function PreparedLayout({ children }: { children: React.ReactNode
             <div className="w-[1800px] h-[800px] border bg-gray-50 shadow rounded-lg overflow-y-scroll">
                 <div>
                     <div className="sticky top-0 z-10">
-                        <div className="bg-red-600 w-full h-full rounded-t-lg md:flex md:justify-between text-center block">
-                            <h2 className="text-md text-white p-4">YA-GO.MX</h2>
+                        <div className="bg-red-600 w-full h-full rounded-t-lg flex justify-center text-center">
+                            <h2 className="text-md text-white p-4 md:mt-0 mt-10">YA-GO.MX</h2>
                         </div>
                         <MainNav />
                     </div>
